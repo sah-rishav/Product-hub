09956884ec217cfb0f4e974f0ade6506afd30724
sah-rishav <sahrishav30@gmail.com>
2021-03-09 10:10:28 +0545

    added remaining pages

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router} from "react-router-dom";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+
+import "./App.css";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+        <div id="container">
+          <div id="header" className="sticky">
+            <Header />
+          </div>
+          {/*<div id = "routes">
+            <Routes />
+    </div>*/}
+    <div id="Home">
+            <Home />
+            </div>
+          <div id="body" />
+          
+          <div id="footer">
+                  
+           <Footer />
+          
+          
+      </div>
+      </div>
+        </Router>
+      </>
+    );
+  }
+}
+
+export default App;
